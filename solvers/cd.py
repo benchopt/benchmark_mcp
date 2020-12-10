0adbac43384b49036e7347eae9e83ea582537511
josephsalmon <joseph.salmon.taff@gmail.com>
2020-12-10 11:58:26 +0100

    tentative mcp cd solver

--- solvers/cd.py
+++ solvers/cd.py
@@ -12,7 +12,6 @@ if import_ctx.failed_import:
     def njit(f):  # noqa: F811
         return f
 
-
 @njit
 def st(x, mu):
     if x > mu:
@@ -22,15 +21,26 @@ def st(x, mu):
     return 0
 
 
+@njit
+def prox_mcp(x, lmbd, gamma):
+    # prox associated to pen = 1/Lj * pen_MCP
+    if x > gamma * lmbd:
+        return x
+    if x < - gamma * lmbd:
+        return x
+    return gamma / (gamma - 1) * st(x, lmbd)
+    # return st(x, lmbd/L)
+
+
 class Solver(BaseSolver):
     name = "cd"
 
     install_cmd = 'conda'
     requirements = ['numba', 'scipy']
 
-    def set_objective(self, X, y, lmbd):
+    def set_objective(self, X, y, lmbd, gamma):
         # use Fortran order to compute gradient on contiguous columns
-        self.X, self.y, self.lmbd = np.asfortranarray(X), y, lmbd
+        self.X, self.y, self.lmbd, self.gamma = np.asfortranarray(X), y, lmbd, gamma
 
         # Make sure we cache the numba compilation.
         self.run(1)
@@ -40,14 +50,14 @@ class Solver(BaseSolver):
         if sparse.issparse(self.X):
             self.w = self.sparse_cd(
                 self.X.data, self.X.indices, self.X.indptr, self.y, self.lmbd,
-                L, n_iter)
+                self.gamma, L, n_iter)
         else:
-            self.w = self.cd(self.X, self.y, self.lmbd, L, n_iter)
+            self.w = self.cd(self.X, self.y, self.lmbd, self.gamma, L, n_iter)
 
     @staticmethod
     @njit
-    def cd(X, y, lmbd, L, n_iter):
-        n_features = X.shape[1]
+    def cd(X, y, lmbd, gamma, L, n_iter):
+        n_samples, n_features = X.shape
         R = np.copy(y)
         w = np.zeros(n_features)
         for _ in range(n_iter):
@@ -55,7 +65,8 @@ class Solver(BaseSolver):
                 if L[j] == 0.:
                     continue
                 old = w[j]
-                w[j] = st(w[j] + X[:, j] @ R / L[j], lmbd / L[j])
+                w[j] = prox_mcp(w[j] + X[:, j] @ R / L[j],
+                                lmbd * np.sqrt(n_samples / L[j]) , gamma)
                 diff = old - w[j]
                 if diff != 0:
                     R += diff * X[:, j]
@@ -63,8 +74,9 @@ class Solver(BaseSolver):
 
     @staticmethod
     @njit
-    def sparse_cd(X_data, X_indices, X_indptr, y, lmbd, L, n_iter):
+    def sparse_cd(X_data, X_indices, X_indptr, y, lmbd, gamma, L, n_iter):
         n_features = len(X_indptr) - 1
+        n_samples = len(y)
         w = np.zeros(n_features)
         R = np.copy(y)
         for _ in range(n_iter):
@@ -76,7 +88,8 @@ class Solver(BaseSolver):
                 scal = 0.
                 for ind in range(start, end):
                     scal += X_data[ind] * R[X_indices[ind]]
-                w[j] = st(w[j] + scal / L[j], lmbd / L[j])
+                w[j] = prox_mcp(w[j] + scal / L[j],
+                                lmbd * np.sqrt(n_samples / L[j]) , gamma)
                 diff = old - w[j]
                 if diff != 0:
                     for ind in range(start, end):
