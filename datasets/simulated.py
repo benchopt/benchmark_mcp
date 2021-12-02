b75de136349512e4523c289ca2d9d671004e9835
josephsalmon <joseph.salmon.taff@gmail.com>
2021-12-02 22:54:41 +0100

    cosmit pep8

--- datasets/simulated.py
+++ datasets/simulated.py
@@ -5,7 +5,6 @@ with safe_import_context() as import_ctx:
     import numpy as np
 
 
-
 class Dataset(BaseDataset):
 
     name = "Simulated"
@@ -13,11 +12,10 @@ class Dataset(BaseDataset):
     # List of parameters to generate the datasets. The benchmark will consider
     # the cross product for each key in the dictionary.
     parameters = {
-        'n_samples, n_features': [
+        "n_samples, n_features": [
             (100, 200),
             # (6, 12)
         ]
-
     }
 
     def __init__(self, n_samples=10, n_features=50, random_state=27):
@@ -32,7 +30,7 @@ class Dataset(BaseDataset):
         X = rng.randn(self.n_samples, self.n_features)
         y = rng.randn(self.n_samples)
         # Normalized by default
-        X /= ((X ** 2).sum(axis=0))
+        X /= (X ** 2).sum(axis=0)
         data = dict(X=X, y=y)
 
         return self.n_features, data
