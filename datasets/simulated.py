FIX objective value, prox computation, normalization. Add sparsity to monitor, normalize param to simulated (#10)

datasets/simulated.py
@@ -14,23 +14,24 @@ class Dataset(BaseDataset):
     parameters = {
         "n_samples, n_features": [
             (100, 200),
-            # (6, 12)
-        ]
+        ],
+        "normalize": [True, False],
     }
 
-    def __init__(self, n_samples=10, n_features=50, random_state=27):
+    def __init__(self, n_samples=10, n_features=50, normalize=False):
         # Store the parameters of the dataset
         self.n_samples = n_samples
         self.n_features = n_features
-        self.random_state = random_state
+        self.normalize = normalize
+        self.random_state = 0
 
     def get_data(self):
 
         rng = np.random.RandomState(self.random_state)
         X = rng.randn(self.n_samples, self.n_features)
         y = rng.randn(self.n_samples)
-        # Normalized by default
-        X /= (X ** 2).sum(axis=0)
+        if self.normalize:
+            X /= np.linalg.norm(X, axis=0)
         data = dict(X=X, y=y)
 
         return self.n_features, data

objective.py
@@ -5,7 +5,7 @@ from benchopt import BaseObjective
 class Objective(BaseObjective):
     name = "MCP Regression"
 
-    parameters = {"reg": [0.1, 0.5], "gamma": [1.2, 1.2]}
+    parameters = {"reg": [1, 0.5, 0.1, 0.01], "gamma": [3]}
 
     def __init__(self, reg=0.1, gamma=1.2):
         self.reg = reg
@@ -16,16 +16,17 @@ class Objective(BaseObjective):
         self.lmbd = self.reg * self._get_lambda_max()
 
     def compute(self, beta):
-        diff = self.y - self.X.dot(beta)
-        pen = (self.lmbd ** 2 * self.gamma / 2.0) * np.ones(beta.shape)
+        diff = self.y - self.X @ beta
+        pen = np.full(len(beta), 0.5 * self.lmbd ** 2 * self.gamma)
         idx = np.abs(beta) <= self.gamma * self.lmbd
-        gamma2 = self.gamma * 2
-        pen[idx] = self.lmbd * np.abs(beta[idx]) - beta[idx] ** 2 / gamma2
+        pen[idx] = (self.lmbd * np.abs(beta[idx]) -
+                    0.5 * beta[idx] ** 2 / self.gamma)
 
-        return diff.dot(diff) / 2.0 + pen.sum()
+        return dict(value=0.5 * diff @ diff + pen.sum(),
+                    sparsity=(beta != 0).sum())
 
     def _get_lambda_max(self):
-        return abs(self.X.T.dot(self.y)).max()
+        return abs(self.X.T @ self.y).max()
 
     def to_dict(self):
         return dict(X=self.X, y=self.y, lmbd=self.lmbd, gamma=self.gamma)

solvers/python_pgd.py
@@ -14,14 +14,23 @@ if import_ctx.failed_import:
 
 @njit
 def prox_mcp_vec(x, lmbd, gamma):
-    st_abs = np.maximum(np.abs(x) - lmbd, 0)
-    return np.sign(x) * np.minimum(np.abs(x), gamma / (gamma - 1) * st_abs)
+    # because of gamma scaling by lipschitz, gamma can be < 1
+    # in which case prox=IHT
+    prox = x.copy()
+    if gamma <= 1:
+        prox[np.abs(x) <= lmbd] = 0.
+    else:
+        idx = np.abs(x) <= gamma * lmbd
+        prox[idx] = gamma / (gamma - 1) * np.sign(x[idx]) * \
+            np.maximum(0, np.abs(x[idx]) - lmbd)
+    return prox
 
 
 class Solver(BaseSolver):
-    name = "Python-PGD"  # proximal gradient, optionally accelerated
+    name = "PGD"  # proximal gradient, optionally accelerated
 
     requirements = ["numba"]
+    # TODO add GIST (Gong et al ICML 2012) to the refs?
     references = [
         'J. Bolte, S. Sabach and M. Teboulle, "Proximal alternating '
         'linearized minimization for nonconvex and nonsmooth problems", '
@@ -33,6 +42,9 @@ class Solver(BaseSolver):
 
     def set_objective(self, X, y, lmbd, gamma):
         self.X, self.y, self.lmbd, self.gamma = X, y, lmbd, gamma
+
+        # cache compilation time for prox_mcp_vec:
+        self.run(1)
 
     def run(self, n_iter):
         if sparse.issparse(self.X):