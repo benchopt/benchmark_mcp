e1fb1098e3df87c877bccfa05a2a5b5f89e972c7
mathurinm <mathurin.massias@gmail.com>
2021-12-03 17:22:03 +0100

    add flash

---
+++ solvers/flashcd.py
@@ -0,0 +1,26 @@
+from benchopt import BaseSolver, safe_import_context
+
+with safe_import_context() as import_ctx:
+    from flashcd import MCP
+    import numpy as np
+
+
+class Solver(BaseSolver):
+    name = "flashcd"
+
+    def set_objective(self, X, y, lmbd, gamma):
+        self.X, self.y = np.asfortranarray(X), y
+        self.lmbd, self.gamma = lmbd, gamma
+        self.clf = MCP(alpha=lmbd / len(y), gamma=gamma *
+                       len(y), fit_intercept=False)
+
+        # Make sure we cache the numba compilation.
+        self.run(1)
+        self.clf.tol = 1e-12
+
+    def run(self, n_iter):
+        self.clf.max_iter = n_iter
+        self.clf.fit(self.X, self.y)
+
+    def get_result(self):
+        return self.clf.coef_
