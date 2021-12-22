fix gradient in objective, cleanup subdiff_dist

objective.py
@@ -3,17 +3,18 @@ from benchopt import BaseObjective
 
 
 def subdiff_distance(w, grad, lmbd, gamma):
+    """Feature-wise distance of gradient to MCP subdifferential."""
     subdiff_dist = np.zeros_like(grad)
     for j in range(len(w)):
         if w[j] == 0:
-            # distance of grad to alpha * [-1, 1]
+            # distance of grad_j to [-lmbd, lmbd]
             subdiff_dist[j] = max(0, np.abs(grad[j]) - lmbd)
         elif np.abs(w[j]) < lmbd * gamma:
-            # distance of grad_j to (alpha - abs(w[j])/gamma) * sign(w[j])
-            subdiff_dist[j] = np.abs(np.abs(grad[j]) - (
-                lmbd - np.abs(w[j])/gamma))
+            # distance of grad_j to {lmbd - abs(w[j])/gamma) * sign(w[j]}
+            subdiff_dist[j] = np.abs(
+                np.abs(grad[j]) - (lmbd - np.abs(w[j]) / gamma))
         else:
-            # distance of grad to 0
+            # distance of grad_j to {0}
             subdiff_dist[j] = np.abs(grad[j])
     return subdiff_dist
 
@@ -21,7 +22,9 @@ def subdiff_distance(w, grad, lmbd, gamma):
 class Objective(BaseObjective):
     name = "MCP Regression"
 
-    parameters = {"reg": [1, 0.5, 0.1, 0.01], "gamma": [3]}
+    parameters = {
+        "reg": [1, 0.5, 0.1, 0.01],
+        "gamma": [3]}
 
     def __init__(self, reg=0.1, gamma=1.2):
         self.reg = reg
@@ -39,11 +42,13 @@ class Objective(BaseObjective):
                     0.5 * beta[idx] ** 2 / self.gamma)
 
         # compute distance of -grad f to subdifferential of MCP penalty
-        grad = self.X.T @ diff / len(self.y)
+        grad = self.X.T @ diff
         opt = subdiff_distance(beta, grad, self.lmbd, self.gamma)
 
         return dict(value=0.5 * diff @ diff + pen.sum(),
-                    sparsity=(beta != 0).sum(), opt=opt.max())
+                    sparsity=(beta != 0).sum(),
+                    opt=opt.max(),
+                    )
 
     def _get_lambda_max(self):
         return abs(self.X.T @ self.y).max()