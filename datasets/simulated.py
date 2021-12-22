fix fault normalization

datasets/simulated.py
@@ -30,6 +30,6 @@ class Dataset(BaseDataset):
         X, y, _ = make_correlated_data(self.n_samples, self.n_features,
                                        rho=self.rho, random_state=rng)
 
-        X /= np.sum(X ** 2, axis=0)  # scale features
+        X /= np.linalg.norm(X, axis=0)
         data = dict(X=X, y=y)
         return self.n_features, data