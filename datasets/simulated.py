a1e00f379366ac0f32cc4fc6986b8ea96eb44740
mathurinm <mathurin.massias@gmail.com>
2021-12-22 16:33:53 +0100

    update simulated dataset, add first order optimality cond to objective

--- datasets/simulated.py
+++ datasets/simulated.py
@@ -1,37 +1,37 @@
-from benchopt import BaseDataset, safe_import_context
+import numpy as np
 
-
-with safe_import_context() as import_ctx:
-    import numpy as np
+from benchopt import BaseDataset
+from benchopt.datasets.simulated import make_correlated_data
 
 
 class Dataset(BaseDataset):
 
     name = "Simulated"
 
-    # List of parameters to generate the datasets. The benchmark will consider
-    # the cross product for each key in the dictionary.
     parameters = {
-        "n_samples, n_features": [
-            (100, 200),
+        'n_samples, n_features': [
+            (100, 500),
         ],
-        "normalize": [True, False],
+        'rho': [0.5],
+        'normalize': [True],
     }
 
-    def __init__(self, n_samples=10, n_features=50, normalize=False):
-        # Store the parameters of the dataset
+    def __init__(
+            self, n_samples=10, n_features=50, rho=0, normalize=True,
+            random_state=27):
         self.n_samples = n_samples
         self.n_features = n_features
+        self.random_state = random_state
+        self.rho = rho
         self.normalize = normalize
-        self.random_state = 0
 
     def get_data(self):
-
         rng = np.random.RandomState(self.random_state)
-        X = rng.randn(self.n_samples, self.n_features)
-        y = rng.randn(self.n_samples)
+
+        X, y, _ = make_correlated_data(self.n_samples, self.n_features,
+                                       rho=self.rho, random_state=rng)
+
         if self.normalize:
             X /= np.linalg.norm(X, axis=0)
         data = dict(X=X, y=y)
-
         return self.n_features, data
--- objective.py
+++ objective.py
@@ -2,6 +2,22 @@ import numpy as np
 from benchopt import BaseObjective
 
 
+def subdiff_distance(w, grad, lmbd, gamma):
+    subdiff_dist = np.zeros_like(grad)
+    for j in range(len(w)):
+        if w[j] == 0:
+            # distance of grad to alpha * [-1, 1]
+            subdiff_dist[j] = max(0, np.abs(grad[j]) - lmbd)
+        elif np.abs(w[j]) < lmbd * gamma:
+            # distance of grad_j to (alpha - abs(w[j])/gamma) * sign(w[j])
+            subdiff_dist[j] = np.abs(np.abs(grad[j]) - (
+                lmbd - np.abs(w[j])/gamma))
+        else:
+            # distance of grad to 0
+            subdiff_dist[j] = np.abs(grad[j])
+    return subdiff_dist
+
+
 class Objective(BaseObjective):
     name = "MCP Regression"
 
@@ -22,8 +38,12 @@ class Objective(BaseObjective):
         pen[idx] = (self.lmbd * np.abs(beta[idx]) -
                     0.5 * beta[idx] ** 2 / self.gamma)
 
+        # compute distance of -grad f to subdifferential of MCP penalty
+        grad = self.X.T @ diff / len(self.y)
+        opt = subdiff_distance(beta, grad, self.lmbd, self.gamma)
+
         return dict(value=0.5 * diff @ diff + pen.sum(),
-                    sparsity=(beta != 0).sum())
+                    sparsity=(beta != 0).sum(), opt=opt.max())
 
     def _get_lambda_max(self):
         return abs(self.X.T @ self.y).max()
