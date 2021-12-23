207e2f7898940b97b4bb375472e2f66e6733853a
mathurinm <mathurin.massias@gmail.com>
2021-12-23 10:18:04 +0100

    standardize > scale

--- datasets/simulated.py
+++ datasets/simulated.py
@@ -15,14 +15,14 @@ class Dataset(BaseDataset):
         "n_samples, n_features": [
             (100, 200),
         ],
-        "standardize": [True, False],
+        "scale": [True, False],
     }
 
-    def __init__(self, n_samples=10, n_features=50, standardize=False):
+    def __init__(self, n_samples=10, n_features=50, scale=False):
         # Store the parameters of the dataset
         self.n_samples = n_samples
         self.n_features = n_features
-        self.standardize = standardize
+        self.scale = scale
         self.random_state = 0
 
     def get_data(self):
@@ -30,7 +30,7 @@ class Dataset(BaseDataset):
         rng = np.random.RandomState(self.random_state)
         X = rng.randn(self.n_samples, self.n_features)
         y = rng.randn(self.n_samples)
-        if self.standardize:
+        if self.scale:
             X /= np.linalg.norm(X, axis=0) / np.sqrt(len(y))
         data = dict(X=X, y=y)
 
