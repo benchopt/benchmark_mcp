214257e9a4f167e73b63821c3ec243e0aba7b9cc
Klopfe <quentin.klopfenstein@u-bourgogne.fr>
2021-12-23 09:24:52 +0100

    Changes in grad computation and first order conditions

--- objective.py
+++ objective.py
@@ -10,8 +10,8 @@ def subdiff_distance(w, grad, lmbd, gamma):
             subdiff_dist[j] = max(0, np.abs(grad[j]) - lmbd)
         elif np.abs(w[j]) < lmbd * gamma:
             # distance of grad_j to (alpha - abs(w[j])/gamma) * sign(w[j])
-            subdiff_dist[j] = np.abs(np.abs(grad[j]) - (
-                lmbd - np.abs(w[j])/gamma))
+            subdiff_dist[j] = np.abs(
+                -grad[j] + lmbd * np.sign(w[j]) - w[j] / gamma)
         else:
             # distance of grad to 0
             subdiff_dist[j] = np.abs(grad[j])
@@ -21,7 +21,7 @@ def subdiff_distance(w, grad, lmbd, gamma):
 class Objective(BaseObjective):
     name = "MCP Regression"
 
-    parameters = {"reg": [1, 0.5, 0.1, 0.01], "gamma": [3]}
+    parameters = {"reg": [0.1, 0.01], "gamma": [1.5]}
 
     def __init__(self, reg=0.1, gamma=1.2):
         self.reg = reg
@@ -32,14 +32,14 @@ class Objective(BaseObjective):
         self.lmbd = self.reg * self._get_lambda_max()
 
     def compute(self, beta):
-        diff = self.y - self.X @ beta
-        pen = np.full(len(beta), 0.5 * self.lmbd ** 2 * self.gamma)
+        diff = self.y - self.X.dot(beta)
+        pen = (self.lmbd ** 2 * self.gamma / 2.) * np.ones(beta.shape)
         idx = np.abs(beta) <= self.gamma * self.lmbd
-        pen[idx] = (self.lmbd * np.abs(beta[idx]) -
-                    0.5 * beta[idx] ** 2 / self.gamma)
+        gamma2 = self.gamma * 2
+        pen[idx] = self.lmbd * np.abs(beta[idx]) - beta[idx] ** 2 / gamma2
 
         # compute distance of -grad f to subdifferential of MCP penalty
-        grad = self.X.T @ diff / len(self.y)
+        grad = self.X.T @ diff
         opt = subdiff_distance(beta, grad, self.lmbd, self.gamma)
 
         return dict(value=0.5 * diff @ diff + pen.sum(),
