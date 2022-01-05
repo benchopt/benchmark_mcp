c258bd1dfbf0695af50697a8f840a6b7356959a0
Klopfe <quentin.klopfenstein@u-bourgogne.fr>
2022-01-05 17:11:48 +0100

    added pycasso solver

---
+++ solvers/pycasso.py
@@ -0,0 +1,29 @@
+from benchopt import BaseSolver, safe_import_context
+
+with safe_import_context() as import_ctx:
+    import pycasso
+    import numpy as np
+    from numpy.linalg import norm
+
+
+class Solver(BaseSolver):
+    name = "pycasso"
+
+    def set_objective(self, X, y, lmbd, gamma):
+        self.X, self.y = np.asfortranarray(X), y
+        self.lmbd, self.gamma = lmbd, gamma
+        lmbd_max = norm(X.T @ y, ord=np.inf) / len(y)
+        self.clf = pycasso.Solver(
+            X, y, lambdas=(1, lmbd / lmbd_max), penalty="mcp", gamma=gamma,
+            useintercept=False, family="gaussian")
+
+        # Make sure we cache the numba compilation.
+        self.run(1)
+        self.clf.prec = 1e-12
+
+    def run(self, n_iter):
+        self.clf.max_ite = n_iter
+        self.clf.train()
+
+    def get_result(self):
+        return self.clf.coef()['beta'].flatten()
