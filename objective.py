e9c4e326f831b0c56df87ed4b468dc9ca5e51c0e
mathurinm <mathurin.massias@gmail.com>
2022-01-25 17:03:32 +0100

    fix typo beta > y

--- objective.py
+++ objective.py
@@ -33,7 +33,7 @@ class Objective(BaseObjective):
         self.lmbd = self.reg * self._get_lambda_max()
 
     def compute(self, beta):
-        diff = self.X @ beta - self.beta
+        diff = self.X @ beta - self.y
         pen = (self.lmbd ** 2 * self.gamma / 2.) * np.ones(beta.shape)
         idx = np.abs(beta) <= self.gamma * self.lmbd
         gamma2 = self.gamma * 2
