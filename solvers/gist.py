60d0b3924f661752acc1cd6067e32ef9c8e38c01
mathurinm <mathurin.massias@gmail.com>
2022-05-23 08:11:54 +0200

    pass on GIST

--- solvers/gist.py
+++ solvers/gist.py
@@ -3,6 +3,7 @@ from benchopt import BaseSolver, safe_import_context
 
 with safe_import_context() as import_ctx:
     import numpy as np
+    from numpy.linalg import norm
     from scipy import sparse
     from numba import njit
 
@@ -25,26 +26,25 @@ def prox_mcp_vec(x, lmbd, gamma):
             np.maximum(0, np.abs(x[idx]) - lmbd)
     return prox
 
+
 @njit
 def mcp_value(w, lmbd, gamma):
     """Compute the value of MCP."""
     s0 = np.abs(w) < gamma * lmbd
     value = np.full_like(w, lmbd*gamma ** 2 / 2.)
-    value[s0] = lmbd * np.abs(w[s0]) - w[s0]**2 / (2 *gamma)
+    value[s0] = lmbd * np.abs(w[s0]) - w[s0]**2 / (2 * gamma)
     return np.sum(value)
 
 
-def current_cost(X, y, w, lmbd, gamma):
+def cost(X, y, w, lmbd, gamma):
     """Compute the following objective function.
 
     min_w 0.5 || y - X w||_2^2 +  \sum_k pen(|w_k|),
 
     where pen is then nonconvex regularizer.
     """
-    normres2 = np.linalg.norm(y - X.dot(w))**2
-    penalty = mcp_value(w,lmbd,gamma)
-    return 0.5 * normres2/X.shape[0] + penalty
-
+    penalty = mcp_value(w, lmbd, gamma)
+    return 0.5 * norm(y - X @ w)**2 / X.shape[0] + penalty
 
 
 class Solver(BaseSolver):
@@ -52,8 +52,8 @@ class Solver(BaseSolver):
 
     requirements = ["numba"]
     references = [
-        ' Gong et al."A General Iterative Shrinkage'
-        ' and Thresholding Algorithm for Non-convex Regularized Optimization Problems"' 
+        ' Gong et al."A General Iterative Shrinkage and Thresholding '
+        'Algorithm for Non-convex Regularized Optimization Problems", '
         "ICML (2013)"
     ]
 
@@ -72,41 +72,37 @@ class Solver(BaseSolver):
     def run(self, n_iter):
         n_samples, n_features = self.X.shape
 
-
         w = np.zeros(n_features)
-
-
-
         not_converged = False
 
-        coutnew = current_cost(self.X, self.y, w, self.lmbd, self.gamma)
-        wp_aux =  np.zeros(n_features,dtype=np.float64)
-        for i in range(n_iter):
+        cost_new = cost(self.X, self.y, w, self.lmbd, self.gamma)
+        wp_aux = np.zeros(n_features, dtype=np.float64)
+        for _ in range(n_iter):
             t = 1
-            #----------------------- gradient update  ------------------------
-            grad = - self.X.T.dot(self.y - self.X.dot(w))/ n_samples
- 
-            wp_aux = prox_mcp_vec(w - grad/t,self.lmbd/t,self.gamma*t)
-            
-            #----------------------- backtracking stepsize ------------------------
-            coutold = coutnew
-            coutnew = current_cost(self.X, self.y, wp_aux, self.lmbd, self.gamma)
-            while coutnew - coutold > - self.sigma / 2 * t * np.linalg.norm(w - wp_aux)**2:
+            # gradient update
+            grad = - self.X.T.dot(self.y - self.X @ w) / n_samples
+
+            wp_aux = prox_mcp_vec(w - grad/t, self.lmbd/t, self.gamma*t)
+
+            # backtracking stepsize
+            cost_old = cost_new
+            cost_new = cost(
+                self.X, self.y, wp_aux, self.lmbd, self.gamma)
+
+            while (cost_new - cost_old >
+                   - self.sigma / 2 * t * norm(w - wp_aux)**2):
                 t = t * self.eta
-                wp_aux = prox_mcp_vec(w - grad/t,self.lmbd/t,self.gamma*t)
-
-
-                coutnew = current_cost(self.X, self.y, wp_aux,self.lmbd,self.gamma)
+                wp_aux = prox_mcp_vec(w - grad/t, self.lmbd/t, self.gamma*t)
+                cost_new = cost(self.X, self.y, wp_aux, self.lmbd, self.gamma)
 
                 if t > self.tmax:
-                    print('Not converging. steps size too small!', t, self.tol)
+                    print('Not converging. Stepsizes too small!', t, self.tol)
                     not_converged = True
                     break
-            w = wp_aux.copy()
+
+            w[:] = wp_aux
             if not_converged:
                 break
-
-
 
         self.w = w
 
